a2e5283dbe5ee0c343e3fdbd744b4ff414a40ba0
RiversCoder <928091232@qq.com>
2019-05-19 21:17:32 +0800

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,104 @@
 
 /*
 		 常用插件地址：
-		 	http://pinkyjie.com/2015/08/02/commonly-used-gulp-plugins-part-1/
+            http://pinkyjie.com/2015/08/02/commonly-used-gulp-plugins-part-1/#more 进击的马斯特 github博客
+
+        常用文档地址：
+            http://i5ting.github.io/stuq-gulp/#10609
  */
 
 let gulp = require("gulp");
 let gulpLoadPlugins = require('gulp-load-plugins');
 let $ = gulpLoadPlugins({lazyload: true, rename:{"gulp-ruby-sass" : "sass", "gulp-markdown-pdf": "mdpdf", "gulp-rev-collector":"revCollector", "gulp-asset-rev":"assetRev"}});
+let del = require("del")
 
-const prefix_css = (done) => {
-    gulp.src("./css/*")
-        .pipe($.rev()) //添加hash后缀
-        .pipe(gulp.dest("./dist/css/")) //移动到dist/css
-        .pipe($.rev.manifest()) //生成文件映射
-        .pipe(gulp.dest("rev/css")) //将映射文件导出到rev/css中
+
+// 打印出多条信息 并且 可以给打印出来的信息着色
+// 文档：https://www.npmjs.com/package/gulp-util （已经弃用未更新，但使用量依旧很大）
+const log = (done) => {
+    console.log(("123","哈哈哈","578","66666")) // 只打印出最后一个
+    $.util.log("123","哈哈哈","578","66666") // gulp-util 出多个 空格隔开
+    $.util.log($.util.colors.blue('123',009,"hello world"));
+    /*
+          可供选择的颜色： $.util.colors.blue
+            red 、green、yellow、blue 、magenta（紫红色）、cyan、white
+            gray ("bright black")、redBright、greenBright、yellowBright、blueBright、
+            magentaBright、cyanBright、whiteBright
+     */
+    /*
+        打印如下：
+
+        C:\web\gulp>gulp
+          [20:39:29] Using gulpfile C:\web\gulp\gulpfile.js
+          [20:39:29] Starting 'default'...
+          66666
+          [20:39:29] 123 哈哈哈 578 66666
+          [20:39:29] 123 9 hello world
+          [20:39:29] Finished 'default' after 202 ms
+
+     */
     done()
 }
 
-const prefix_js = (done) => {
-    gulp.src("./js/*.js")
-        .pipe($.rev())
-        .pipe(gulp.dest('./dist/js/'))
-        .pipe($.rev.manifest())
-        .pipe(gulp.dest('rev/js'));
+
+// 删除指定文件夹或者文件 类似于gulp-clean
+// 文档：https://www.npmjs.com/package/del
+const dels = (done) => {
+  (async (done) => {
+    const deletedPaths = await del(['./dest/html/*'], {dryRun: true});
+    console.log('Files and folders that would be deleted:\n', deletedPaths.join('\n'));
     done()
+  })(done);
+  /*
+      打印如下：
+
+      C:\web\gulp (master -> origin)
+      λ gulp
+      [20:47:01] Using gulpfile C:\web\gulp\gulpfile.js
+      [20:47:01] Starting 'default'...
+      Files and folders that would be deleted:
+       C:\web\gulp\dest\html\index.html
+      [20:47:01] Finished 'default' after 24 ms
+
+   */
 }
 
-const prefix_image = (done) => {
-    gulp.src("./images/*.jpg")   //该任务针对的文件
-      .pipe($.rev())  //该任务调用的模块
-      .pipe(gulp.dest('dist/images/')) //编译后的路径
-      .pipe($.rev.manifest())
-      .pipe(gulp.dest('rev/images'));
-    done()
+// 计算文件的大小 通常与压缩类工具放在一起实用
+// https://www.npmjs.com/package/gulp-bytediff
+const bytediff = (done) => {
+  gulp.src("./images/*.jpg")
+    .pipe($.bytediff.start())
+    .pipe($.imagemin())
+    .pipe($.bytediff.stop(func_stop))
+    .pipe(gulp.dest("./dist/images/"))
+  done()
 }
 
-const prefix_html = (done) => {
-    gulp.src(["rev/**/*.json","./*.html"])
-        .pipe($.revCollector())
-        .pipe(gulp.dest('./'))
-    done()
+function func_stop(data){
+  console.log(data)
 }
 
-exports.default = gulp.series(prefix_css,  prefix_js, prefix_image, prefix_html)
+// 打印结果：
+/*
+  C:\web\gulp (master -> origin)
+  λ gulp
+  [21:00:28] Using gulpfile C:\web\gulp\gulpfile.js
+  [21:00:28] Starting 'default'...
+  [21:00:29] Finished 'default' after 466 ms
+  { fileName: '01.jpg',
+    startSize: 140991,
+    endSize: 139289,
+    savings: 1702,
+    percent: 0.9879283074806193 }
+  [21:00:30] undefined
+  { fileName: '02.jpg',
+    startSize: 339094,
+    endSize: 342126,
+    savings: -3032,
+    percent: 1.008941473455738 }
+  [21:00:30] undefined
+  [21:00:30] gulp-imagemin: Minified 1 image (saved 1.7 kB - 1.2%)
 
+ */
 
-
+exports.default = bytediff
